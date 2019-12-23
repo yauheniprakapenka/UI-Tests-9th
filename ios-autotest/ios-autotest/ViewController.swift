932433babf99d5129718d2b440d2cc277a5931d1
Yauheni Prakapenka <eprakopenko@intervale.ru>
2019-12-24 01:29:35 +0300

    init project

---
+++ ios-autotest/ios-autotest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-autotest
+//
+//  Created by yauheni prakapenka on 24.12.2019.
+//  Copyright © 2019 yauheni prakapenka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
