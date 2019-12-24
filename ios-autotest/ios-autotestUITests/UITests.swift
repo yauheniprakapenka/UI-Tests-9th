7c0dcf3a7e1607493039a12ef76c5f42ff15fafe
Yauheni Prakapenka <eprakopenko@intervale.ru>
2019-12-24 09:09:14 +0300

    Added UI tests

---
+++ ios-autotest/ios-autotestUITests/UITests.swift
@@ -0,0 +1,14 @@
+//
+//  UITests.swift
+//  ios-autotestUITests
+//
+//  Created by yauheni prakapenka on 24.12.2019.
+//  Copyright © 2019 yauheni prakapenka. All rights reserved.
+//
+
+import XCTest
+
+class UITests: UITestCase {
+    
+
+}
