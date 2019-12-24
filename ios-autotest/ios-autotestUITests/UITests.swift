b27f8f20c9e53de19d53359e3943630b2c2b85ef
Yauheni Prakapenka <eprakopenko@intervale.ru>
2019-12-24 09:31:17 +0300

    Refactoring

--- ios-autotest/ios-autotestUITests/UITests.swift
+++ ios-autotest/ios-autotestUITests/UITests.swift
@@ -10,5 +10,12 @@ import XCTest
 
 class UITests: UITestCase {
     
+    func testLaunchPerformance() {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
 
 }
