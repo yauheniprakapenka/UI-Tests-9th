fab7f39ef611e089daa0e8c5e17d52d4c6a339b5
Yauheni Prakapenka <eprakopenko@intervale.ru>
2020-01-06 14:56:48 +0300

    Added  collection view

--- ios-autotest/ios-autotest/Controller/ViewController.swift
+++ ios-autotest/ios-autotest/Controller/ViewController.swift
@@ -14,15 +14,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var withoutArgumentLabel: UILabel!
     @IBOutlet weak var hiddenLabel: UILabel!
-    
     @IBOutlet weak var buttonOne: UIButton!
     @IBOutlet weak var buttonTwo: UIButton!
     @IBOutlet weak var buttobThree: UIButton!
-    
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
-    var countTimer = 0
-    let locationManager = CLLocationManager()
+    private let locationManager = CLLocationManager()
+    private let textArray = ["Утренний рассвет", "Вечерний закат", "Дивный день"]
+    private let imagesArray = [#imageLiteral(resourceName: "tumblr_ol6o615mfP1qa9gmgo10_500"), #imageLiteral(resourceName: "tumblr_ol6o615mfP1qa9gmgo1_500"), #imageLiteral(resourceName: "tumblr_ol6o615mfP1qa9gmgo6_500")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,22 +29,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         
-        if ProcessInfo.processInfo.arguments.contains("UITesting") {
-            withoutArgumentLabel.text = "👩‍🔬💉 Режим ui тестирования"
-            withoutArgumentLabel.textColor = .red
-            activityIndicator.alpha = 1
-        }
+        addArgument()
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
         resultLabel.text = "\(sender.currentTitle ?? "") нажата"
-        
         let buttons = [buttonOne, buttonTwo, buttobThree]
-        
         for button in buttons {
             button?.backgroundColor = #colorLiteral(red: 0.0759813115, green: 0.6622361541, blue: 0.9625448585, alpha: 1)
         }
-        
         sender.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
     }
     
@@ -62,15 +54,44 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     @IBAction func coverInterfaceButtonTapped(_ sender: UIButton) {
-        buttonOne.isEnabled = false
-        makeView()
-    }
-    
-    private func makeView() {
         let customView = UIView()
         customView.frame = CGRect(x: 0, y: 50, width: UIScreen.main.bounds.width, height: 100)
         customView.backgroundColor = .red
         view.addSubview(customView)
     }
     
+    private func addArgument() {
+        if ProcessInfo.processInfo.arguments.contains("UITesting") {
+            withoutArgumentLabel.text = "👩‍🔬💉 Режим ui тестирования"
+            withoutArgumentLabel.textColor = .red
+            activityIndicator.alpha = 1
+        }
+    }
+}
+
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        textArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCollectionViewCell
+        
+        cell.label.text = textArray[indexPath.row]
+        cell.label.textColor = .white
+        
+        cell.layer.borderColor = UIColor.black.cgColor
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 8
+
+        cell.imageView.image = imagesArray[indexPath.row]
+        cell.imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: UIScreen.main.bounds.width - 40, height: 50.0)
+    }
 }
